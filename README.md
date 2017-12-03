813c1206ab641035244ad15aa39fd7ecdd39a1b0
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2017-12-03 13:53:59 +0100

    Adding modal, README and cleaning code

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
-frontend-nanodegree-arcade-game
+Arcade-game
 ===============================
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+This is the third project of the intermediate JavaScript section for Udacity's front-end web development nanodegree. It's a version of the classic frogger arcade-game. The basic requirements for this projects are:
+- The player has to reach the water without colliding into the enemies (bugs).
+- The player can move up, down, left and right without falling out of the screen.
+- The enemies run on the paved block portion of the scene at various speeds.
+- The game resets when the player collides into an enemy.
+- When the player reaches the water the game is won.
+Here is the complete [rubric](https://review.udacity.com/#!/projects/2696458597/rubric).
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+### What I've learned
+This project's goal is to learn about object-oriented programming. All functions must be either class functions or class prototype functions.
+I've learned how to write coherent class prototype functions as well as how the scope and prototype chains works.
+I've also learned how to use canvas to draw patterns, text and how to create animated elements.
+
+### How to install it.
+To run the application clone the repository to your computer:
+
+```
+git clone https://github.com/Fa-v/arcade-game.git
+cd arcade-game/
+```
+Then open the index html in your browser.
+
+You can also follow this link to play it.
+
+### How to play it.
+Use the following keys to move the player:
+- Up: `up-arrow` or `W`
+- Down: `down-arrow` or `S`
+- Left: `left-arrow` or `A`
+- Right: `right-arrow` or `D`
+
+You have to get to the water without colliding with the ladybugs. You have three lives, you lose a life when you collide three times. If you lose three lives you'll lose the game. Each time you get to the water you'll score 100 points. Each time you collide you'll lose 50 points. After you score 300 points collectibles will appear. You'll win the game when you score at least 2000 points.
+When you lose or win the game, a play-again button will appear.
+
+### Useful links.
+These are some sites that helped me to work on this project:
+- w3school [Game Movement](https://www.w3schools.com/graphics/game_movement.asp)
+- Khan Academy [Intro to Hoppy Beaver](https://www.khanacademy.org/computing/computer-programming/programming-games-visualizations/side-scroller/a/intro-to-hoppy-beaver)
+- HTML5 Canvas [Chapter 3. The HTML5 Canvas Text API](http://chimera.labs.oreilly.com/books/1234000001654/ch03.html). By Steve Fulton and Jeff Fulton
+- Dive into HTML5 [Let’s Call It A Draw(ing Surface)](http://diveintohtml5.info/canvas.html#text). By Mark Pilgrim
+- Udacity's [HTML5 Canvas](https://www.udacity.com/course/html5-canvas--ud292)
--- js/app.js
+++ js/app.js
@@ -3,87 +3,87 @@
  * @param {Number} y
  */
 var Enemy = function (y) {
-    this.initialPosition = -100;
-    this.maxX = 600;
-    this.y = y;
-    this.minSpeed = 60;
-    this.maxSpeed = 260;
-    this.speed = this.calculateSpeed();
-    this.sprite = 'images/enemy-bug.png';
+  this.initialPosition = -100;
+  this.maxX = 600;
+  this.y = y;
+  this.minSpeed = 60;
+  this.maxSpeed = 260;
+  this.speed = this.calculateSpeed();
+  this.sprite = 'images/enemy-bug.png';
 
-    this.getInitialPosition();
+  this.getInitialPosition();
 };
 
 /**
  * @description Sets the enemy's initial coordinates
  */
 Enemy.prototype.getInitialPosition = function () {
-    this.x = this.initialPosition;
-    this.y = this.y;
+  this.x = this.initialPosition;
+  this.y = this.y;
 }
 
 /**
  * @description Calculates and returns a random speed
  */
 Enemy.prototype.calculateSpeed = function (minSpeed, maxSpeed) {
-    let randomSpeed;
+  let randomSpeed;
 
-    if (game.score >= 1000) {
-        this.minSpeed = 150;
-        this.maxSpeed = 350;
-    }
-    if (game.score >= 1500) {
-        this.minSpeed = 200;
-        this.maxSpeed = 500;
-    }
+  if (game.score >= 1000) {
+    this.minSpeed = 150;
+    this.maxSpeed = 350;
+  }
+  if (game.score >= 1500) {
+    this.minSpeed = 200;
+    this.maxSpeed = 500;
+  }
 
-    randomSpeed = Math.random() * (this.maxSpeed - this.minSpeed) + this.minSpeed;
-    randomSpeed = Math.floor(randomSpeed);
-    randomSpeed < 100 ? randomSpeed = 100 : randomSpeed;
-    return this.speed = randomSpeed;
+  randomSpeed = Math.random() * (this.maxSpeed - this.minSpeed) + this.minSpeed;
+  randomSpeed = Math.floor(randomSpeed);
+  randomSpeed < 100 ? randomSpeed = 100 : randomSpeed;
+  return this.speed = randomSpeed;
 }
 
 /**
  * @description Updates the enemy's position, multiplying the dt parameter by
- * enemy's movement ensure the game runs at the same speed for all computers
+ * enemy's movement ensures the game runs at the same speed for all computers
  * @param {Number} dt dt, a time delta between ticks
  */
 Enemy.prototype.update = function (dt) {
-    this.x += this.speed * dt;
-    if (this.x >= this.maxX) {
-        this.x = this.initialPosition;
-        this.speed = this.calculateSpeed();
-    }
+  this.x += this.speed * dt;
+  if (this.x >= this.maxX) {
+    this.x = this.initialPosition;
+    this.speed = this.calculateSpeed();
+  }
 };
 
 /**
  * @description Draw the enemy on the screen
  */
 Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 /**
  * @description Sets all variables necessary to create the player
  */
 let Player = function () {
-    this.minX = 0;
-    this.maxX = 400;
-    this.minY = 0;
-    this.maxY = 400;
-    this.isInWater = false;
-    this.sprite = 'images/char-boy.png';
+  this.minX = 0;
+  this.maxX = 400;
+  this.minY = 0;
+  this.maxY = 400;
+  this.isInWater = false;
+  this.sprite = 'images/char-boy.png';
 
-    this.getInitialPosition()
+  this.getInitialPosition();
 }
 
 /**
  * @description Sets the player's initial coordinates
  */
 Player.prototype.getInitialPosition = function() {
-    this.x = 200;
-    this.y = 400;
-    this.isInWater;
+  this.x = 200;
+  this.y = 400;
+  this.isInWater;
 }
 
 /**
@@ -91,43 +91,43 @@ Player.prototype.getInitialPosition = function() {
  * checkCollisions methods
  */
 Player.prototype.update = function () {
-    if (this.y <= -10) {
-        this.getInitialPosition();
-        !this.isInWater && game.updateScore();
-    }
-    this.checkCollisions();
+  if (this.y <= -10) {
+    this.getInitialPosition();
+    !this.isInWater && game.updateScore();
+  }
+  this.checkCollisions();
 }
 
 /**
  * @description Draw the player on the screen
  */
 Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 /**
  * @description Checks if the enemy and the player are on the same coordinates
  */
 Player.prototype.checkCollisions = function () {
-    allEnemies.forEach(enemy => {
-        let playerOnFirstRow = player.y - 14 === enemy.y;
-        let playerOnSecondRow =  player.y - 16 === enemy.y;
-        let playerOnThirdRow = player.y - 20 === enemy.y;
-        let enemyOnPlayer = enemy.x >= player.x - 40 && enemy.x <= player.x + 40;
+  allEnemies.forEach(enemy => {
+    let playerOnFirstRow = player.y - 14 === enemy.y;
+    let playerOnSecondRow =  player.y - 16 === enemy.y;
+    let playerOnThirdRow = player.y - 20 === enemy.y;
+    let enemyOnPlayer = enemy.x >= player.x - 40 && enemy.x <= player.x + 40;
 
-        let collision = (playerOnFirstRow || playerOnSecondRow || playerOnThirdRow)
-            && enemyOnPlayer;
+    let collision = (playerOnFirstRow || playerOnSecondRow || playerOnThirdRow)
+      && enemyOnPlayer;
 
-        collision && this.getInitialPosition();
-        collision && game.score >= 50 && (game.score -= 50);
-        collision && (game.collisions += 1);
+    collision && this.getInitialPosition();
+    collision && game.score >= 50 && (game.score -= 50);
+    collision && (game.collisions += 1);
 
-        if (game.collisions === 3) {
-            allLives.splice(0, 1);
-            game.collisions = 0;
-        }
-        collision && allLives.length === 0 && (game.isOver = true) && game.over();
-    });
+    if (game.collisions === 3) {
+      allLives.splice(0, 1);
+      game.collisions = 0;
+    }
+    collision && allLives.length === 0 && (game.isOver = true) && game.over();
+  });
 }
 
 /**
@@ -135,20 +135,20 @@ Player.prototype.checkCollisions = function () {
  * @param {Object} allowedKeys
  */
 Player.prototype.handleInput = function (allowedKeys) {
-    switch (allowedKeys) {
-        case 'left':
-            this.x <= 10 ? this.x = 0 : this.x -= 100;
-            break;
-            case 'up':
-            this.y <= 50 ? this.y = -10 : this.y -= 80;
-            break;
-        case 'right':
-            this.x >= 400 ? this.x = 400 : this.x += 100;
-            break;
-            case 'down':
-            this.y >= 400 ? this.y = 400 : this.y += 80;
-            break;
-    }
+  switch (allowedKeys) {
+    case 'left':
+      this.x <= 10 ? this.x = 0 : this.x -= 100;
+      break;
+    case 'up':
+      this.y <= 50 ? this.y = -10 : this.y -= 80;
+      break;
+    case 'right':
+      this.x >= 400 ? this.x = 400 : this.x += 100;
+      break;
+    case 'down':
+      this.y >= 400 ? this.y = 400 : this.y += 80;
+      break;
+  }
 }
 
 /**
@@ -157,28 +157,28 @@ Player.prototype.handleInput = function (allowedKeys) {
  * @param {Object} event
  */
 document.addEventListener('keyup', function (e) {
-    var allowedKeys = {
-        37: 'left',
-        65:'left',
-        38: 'up',
-        87: 'up',
-        39: 'right',
-        68: 'right',
-        40: 'down',
-        83: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    65:'left',
+    38: 'up',
+    87: 'up',
+    39: 'right',
+    68: 'right',
+    40: 'down',
+    83: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 /**
  * @description Tracks the player's score
  */
 let Game = function () {
-    this.score = 0;
-    this.higherScore = 0;
-    this.collisions = 0;
-    this.isOver = false;
+  this.score = 0;
+  this.higherScore = 0;
+  this.collisions = 0;
+  this.isOver = false;
 }
 
 /**
@@ -186,12 +186,12 @@ let Game = function () {
  * is won
  */
 Game.prototype.updateScore = function () {
-    !player.isInWater && (this.score += 100);
+  !player.isInWater && (this.score += 100);
 
-    if (!player.isInWater && this.score >= 2000 && allLives.length > 0) {
-        this.playerWon = true;
-        this.won();
-    }
+  if (!player.isInWater && this.score >= 2000 && allLives.length > 0) {
+    this.playerWon = true;
+    this.won();
+  }
 }
 
 /**
@@ -199,183 +199,215 @@ Game.prototype.updateScore = function () {
  * functions
  */
 Game.prototype.over = function () {
-    let button = document.getElementById('button');
+  let button = document.getElementById('button');
 
-    button.style.visibility = 'visible';
-    this.isOver === true && gameRecords.push(this.score);
+  button.style.visibility = 'visible';
+  this.isOver === true && gameRecords.push(this.score);
 
-    this.setHistoryGame();
-    this.sortScore();
+  this.setHistoryGame();
+  this.sortScore();
 }
 
 /**
- * @description When player wins, push game score to  gameRecord and calls
+ * @description When player wins, pushes game score to gameRecord and calls
  * setHistoryGame and sortScore functions
  */
 Game.prototype.won = function () {
-    let button = document.getElementById('button');
+  let button = document.getElementById('button');
 
-    button.style.visibility = 'visible';
+  button.style.visibility = 'visible';
 
-    this.playerWon === true && gameRecords.push(this.score);
-    this.setHistoryGame();
-    this.sortScore();
+  this.playerWon === true && gameRecords.push(this.score);
+  this.setHistoryGame();
+  this.sortScore();
 
-    this.gameFinished = true;
+  this.gameFinished = true;
 }
 
 /**
  * @description Sorts game records and returns the higher score
  */
 Game.prototype.sortScore = function () {
-    gameRecords.length && gameRecords.sort(function (a, b) {
-        return (b - a);
-    });
-    this.higherScore += gameRecords[0]
+  gameRecords.length && gameRecords.sort(function (a, b) {
+    return (b - a);
+  });
+  this.higherScore += gameRecords[0];
 }
 
 /**
  * @description Sets game score in local storage
  */
 Game.prototype.setHistoryGame = function () {
-    localStorage.setItem('record', JSON.stringify(gameRecords));
+  localStorage.setItem('record', JSON.stringify(gameRecords));
 }
 
 /**
  * @description Resets all values for a new game
  */
 Game.prototype.resetGame = function () {
-    game = new Game();
-    enemy1 = new Enemy(60);
-    enemy2 = new Enemy(144);
-    enemy3 = new Enemy(226);
-    allEnemies = [enemy1, enemy2, enemy3];
-    player.getInitialPosition();
+  game = new Game();
+  enemy1 = new Enemy(60);
+  enemy2 = new Enemy(144);
+  enemy3 = new Enemy(226);
+  allEnemies = [enemy1, enemy2, enemy3];
+  player.getInitialPosition();
 
-    life1 = new Lives(370);
-    life2 = new Lives(410);
-    life3 = new Lives(450);
-    allLives = [life1, life2, life3];
+  life1 = new Lives(370);
+  life2 = new Lives(410);
+  life3 = new Lives(450);
+  allLives = [life1, life2, life3];
 }
 
 /**
  * @description Creates the text for game's score on canvas
  */
 Game.prototype.render = function () {
-    ctx.font = '36px sans-serif';
+  ctx.font = '36px sans-serif';
+  ctx.fillStyle = 'white';
+  ctx.fillText(`Score: ${this.score}`, 20, 563);
+  ctx.textBaseline = 'middle';
+
+  if (this.isOver === true) {
+    ctx.fillStyle = '#eec643';
+    ctx.fillRect(0, 125, 505, 260);
+    ctx.font = 'bold 30px sans-serif';
+    ctx.fillStyle = 'black';
+    ctx.fillText(`Game Over!`, 100, 220);
+    ctx.fillText(`Score: ${this.score}`, 100, 260);
+    ctx.fillText(`Higher Score: ${this.higherScore}`, 100, 300);
+  }
+
+  if (this.gameFinished === true) {
+    ctx.fillStyle = '#f02d3a';
+    ctx.fillRect(0, 125, 505, 260);
+    ctx.font = 'bold 30px sans-serif';
     ctx.fillStyle = 'white';
-    ctx.fillText(`Score: ${this.score}`, 20, 563);
-    ctx.textBaseline = 'middle';
-
-    if (this.isOver === true) {
-        ctx.fillStyle = '#eec643';
-        ctx.fillRect(0, 125, 505, 260);
-        ctx.font = 'bold 30px sans-serif';
-        ctx.fillStyle = 'black';
-        ctx.fillText(`Game Over!`, 100, 220);
-        ctx.fillText(`Score: ${this.score}`, 100, 260);
-        ctx.fillText(`Higher Score: ${this.higherScore}`, 100, 300);
-    }
-
-    if (this.gameFinished === true) {
-        ctx.fillStyle = '#f02d3a';
-        ctx.fillRect(0, 125, 505, 260);
-        ctx.font = 'bold 30px sans-serif';
-        ctx.fillStyle = 'white';
-        ctx.fillText(`You Won!`, 100, 220);
-        ctx.fillText(`Score: ${this.score}`, 100, 260);
-        ctx.fillText(`Higher Score: ${this.higherScore}`, 100, 300);
-    }
+    ctx.fillText(`You Won!`, 100, 220);
+    ctx.fillText(`Score: ${this.score}`, 100, 260);
+    ctx.fillText(`Higher Score: ${this.higherScore}`, 100, 300);
+  }
 }
 
+/**
+ * @description Sets all variables necessary to create lives
+ * @param {number} x coordinate to place the hearts
+ */
 let Lives = function (x) {
-    this.sprite = 'images/Heart.png';
-    this.x = x;
-    this.y = 530;
-    this.width = 40;
-    this.height = 60;
+  this.sprite = 'images/Heart.png';
+  this.x = x;
+  this.y = 530;
+  this.width = 40;
+  this.height = 60;
 }
 
+/**
+ * @description Draws the hearts on canvas
+ */
 Lives.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
 }
 
+/**
+ * @description Sets all variables necessary for collectibles and calls getRandomGem
+ */
 let Gem = function () {
-    this.x;
-    this.y;
-    this.width = 60;
-    this.height = 80;
-    this.gemImages = [
-        'images/Gem Blue.png',
-        'images/Gem Green.png',
-        'images/Gem Orange.png',
-        'images/Star.png',
-        'images/Key.png'
-    ];
-    this.gemValues = [100, 150, 200, 300, 350];
-    this.randomGem;
-    this.sprite;
+  this.x;
+  this.y;
+  this.width = 60;
+  this.height = 80;
+  this.gemImages = [
+    'images/Gem Blue.png',
+    'images/Gem Green.png',
+    'images/Gem Orange.png',
+    'images/Star.png',
+    'images/Key.png'
+  ];
+  this.gemValues = [100, 150, 200, 300, 350];
+  this.randomGem;
+  this.sprite;
 
-    this.getRandomGem();
+  this.getRandomGem();
 }
 
+/**
+ * @description Creates random values to create collectible items
+ */
 Gem.prototype.getRandomGem = function () {
-    let randomX,
-        randomY,
-        possibleX = [20, 120, 220, 320, 420],
-        possibleY = [120, 200, 280];
+  let randomX,
+      randomY,
+      possibleX = [20, 120, 220, 320, 420],
+      possibleY = [120, 200, 280];
 
-    this.randomGem = Math.floor(Math.random() * this.gemImages.length);
-    randomX = Math.floor(Math.random() * possibleX.length);
-    randomY = Math.floor(Math.random() * possibleY.length);
+  this.randomGem = Math.floor(Math.random() * this.gemImages.length);
+  randomX = Math.floor(Math.random() * possibleX.length);
+  randomY = Math.floor(Math.random() * possibleY.length);
 
-    this.x = possibleX[randomX];
-    this.y = possibleY[randomY];
-    this.gemValues = this.gemValues[this.randomGem];
-    this.sprite = this.gemImages[this.randomGem];
+  this.x = possibleX[randomX];
+  this.y = possibleY[randomY];
+  this.gemValues = this.gemValues[this.randomGem];
+  this.sprite = this.gemImages[this.randomGem];
 }
 
+/**
+ * @description Instantiates collectible items
+ */
 Gem.generateGem = function () {
-    let gem = new Gem();
+  let gem = new Gem();
 
-    allGems.push(gem);
-    Gem.getGems();
+  allGems.push(gem);
+  Gem.getGems();
 }
 
+/**
+ * @description Sets a random delay time for showing and removing collectible items
+ */
 Gem.getGems = function () {
-    let delay = Math.floor(Math.random() * (10000 - 6000) + 6000);
+  let delay = Math.floor(Math.random() * (10000 - 6000) + 6000);
 
-    setTimeout(Gem.removeGem, 6000);
-    setTimeout(Gem.generateGem, delay);
+  setTimeout(Gem.removeGem, 6000);
+  setTimeout(Gem.generateGem, delay);
 }
 
+/**
+ * @description Removes collectibles from canvas
+ */
 Gem.removeGem = function () {
-    allGems.splice(0, 1);
+  allGems.splice(0, 1);
 }
 
+/**
+ * @description Clears timeout functions
+ */
 Gem.clearTimeOuts = function () {
-    clearTimeout(Gem.generateGem);
-    clearTimeout(Gem.removeGem);
+  clearTimeout(Gem.generateGem);
+  clearTimeout(Gem.removeGem);
 }
 
+/**
+ * @description Draws collectibles on canvas
+ */
 Gem.prototype.render = function () {
-    if (game.score >= 300) {
-        ctx.font = 'bold 20px sans-serif';
-        ctx.fillStyle = 'grey';
-        ctx.fillText(`${this.gemValues}`, this.x + 12, this.y + 90);
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
-    }
+  if (game.score >= 300) {
+    ctx.font = 'bold 20px sans-serif';
+    ctx.fillStyle = 'grey';
+    ctx.fillText(`${this.gemValues}`, this.x + 12, this.y + 90);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.height);
+  }
 }
 
+/**
+ * @description Tracks when the player is on the same coordinates as a collectible and
+ * increases the score
+ */
 Gem.prototype.update = function () {
-    let gemPicked = (allGems.length && player.y + 40 === allGems[0].y) &&
-    (allGems.length && player.x + 20 === allGems[0].x);
+  let gemPicked = (allGems.length && player.y + 40 === allGems[0].y) &&
+  (allGems.length && player.x + 20 === allGems[0].x);
 
-    /* the if statement prevents to increase the score before the gem shows up on the screen */
-    if (gemPicked) {
-        (game.score < 300) ? game.score = game.score : (game.score += this.gemValues);
-        allGems.splice(0, 1);
-    }
+  /* the if statement prevents increasing the score before the gem shows up on the screen */
+  if (gemPicked) {
+    (game.score < 300) ? game.score = game.score : (game.score += this.gemValues);
+    allGems.splice(0, 1);
+  }
 }
 
 /**
